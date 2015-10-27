ea10c7877e772666e43ee8de92f35469204fdf4d
alu0100818369 <alu0100818369@ull.edu.es>
2015-10-27 17:35:37 +0000

    Añadiendo la suma

--- lib/fraccion.rb
+++ lib/fraccion.rb
@@ -1,8 +1,20 @@
+require_relative 'gcd'
 class Fraccion
+	attr_reader :x, :y
 	def initialize(x,y)
 		@x,@y=x,y
 	end
 	def to_s
 		"#{@x}/#{@y}"
+	end
+	def +(other)
+		a=@x*other.y+other.x*@y
+		b=@y*other.y
+		c=gcd(a,b)
+		if c!=1
+			a=a/c
+			b=b/c
+		end
+		Fraccion.new(a,b)
 	end
 end
--- test/tc_fraccion.rb
+++ test/tc_fraccion.rb
@@ -4,8 +4,13 @@ require 'test/unit'
 class TestFraccion < Test::Unit::TestCase
 	def setup
 		@f1 = Fraccion.new(1,1)
+		@f2 = Fraccion.new(1,2)
+		@f3 = Fraccion.new(1,4)
 	end
 	def test_simple
 		assert_equal("1/1",  @f1.to_s)
 	end
+	def test_suma
+		assert_equal("3/4", (@f2+(@f3)).to_s)
+	end 
 end
